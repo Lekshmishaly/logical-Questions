5149b0506005c6fa1a2294b119e220e1715b37cc
Lekshmishaly <160052455+Lekshmishaly@users.noreply.github.com>
2024-09-26 18:20:53 +0530

    Create Calculate the sum of an object.js

---
+++ Calculate the sum of an object.js
@@ -0,0 +1,25 @@
+let x={
+    a:[1,2,3,4,5],
+    b:4,
+     c:{ 
+         d:[2,3,4,5],
+     }
+}
+
+function findSum(obj){
+    let sum = 0;
+    for(let key in obj){
+        if(typeof obj[key] == "number"){
+            sum = sum+obj[key];
+        }else if(Array.isArray(obj[key])){
+            obj[key].forEach((el)=>{
+                sum = sum+el;
+            })
+        }else if(typeof obj[key] == "object"){
+            sum = sum + findSum(obj[key]);
+        }
+    }
+    return sum;
+}
+
+console.log(findSum(x))
