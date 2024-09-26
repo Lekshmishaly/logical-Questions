5855bfcd7bc53f58a4be098439f8a2efff875e02
Lekshmishaly <160052455+Lekshmishaly@users.noreply.github.com>
2024-09-26 20:40:06 +0530

    Update second largest in an array.js

--- second largest in an array.js
+++ second largest in an array.js
@@ -1,1 +1,21 @@
-jyghfhfhf
+const num=[67,23,90,45,78,22,110];
+
+function test(number){
+ return  number.reduce(([largeNum,secLarge],curr)=>{
+    if(secLarge<curr){
+        largeNum=secLarge;
+        secLarge=curr;
+    }else if(secLarge<curr){
+        secLarge=curr;
+    }
+    
+    return  [largeNum,secLarge] ;
+},[-Infinity,-Infinity])    
+};
+
+const result=(test(num))
+
+console.log({
+    SecondLargest:result[0],
+    Largest:result[1],
+})
