d4b45ce3b9be55ed4e04bb995107662a813ad6ec
Lekshmishaly <160052455+Lekshmishaly@users.noreply.github.com>
2024-09-26 18:19:42 +0530

    Update Sorting objects.js

--- Sorting objects.js
+++ Sorting objects.js
@@ -1,1 +1,22 @@
-ytytgyjg
+const x = {
+    a:5,
+    b:78,
+    c:71,
+    d:4
+}
+function test(obj){
+    
+    let array=Object.values(obj);
+    array=array.sort((a,b)=>a-b);
+    
+    const result={};
+    for(let i=0;i<array.length;i++){
+        for(let keys in obj){
+            if(array[i]===obj[keys]){
+              result[keys]= array[i]; 
+            }
+        }
+    }
+    return result
+}
+console.log(test(x))
